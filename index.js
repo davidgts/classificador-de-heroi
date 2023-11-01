af6e75557602c81d5b2160138d854ae33a8b68af
davidgts <david.ms96@hotmail.com>
2023-10-31 21:35:15 -0300

    Desafio Finalizado

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.info('Olá Mundão');
+const tablePlayers = [
+    {"nome": "Fulano","xp":2500},
+    {"nome": "Beltrano","xp":950},
+    {"nome": "Sicrano","xp":8300},
+    {"nome": "Pedrinho","xp":15000},
+    {"nome": "Maria","xp":6300},
+    {"nome": "Luiza","xp":7650},
+]
+
+console.log('| Tabela de Heróis 🎮 |')
+console.table(tablePlayers,["nome","xp"])
+
+tablePlayers.forEach((e)=>{
+    let level = ''
+    if(e.xp<=1000){ // No caso de ser 1000 de xp o Herói não teria nenhum nível
+        level = 'Ferro'
+    }else if(e.xp<=2000){
+        level = 'Bronze'
+    }else if(e.xp<=5000){
+        level = 'Prata'
+    }else if(e.xp<=7000){
+        level = 'Ouro'
+    }else if(e.xp<=8000){
+        level = 'Platina'
+    }else if(e.xp<=9000){
+        level = 'Ascendente'
+    }else if(e.xp<=10000){
+        level = 'Imortal'
+    }else{
+        level='Radiante'
+    } 
+    
+    
+    console.log(`O herói de nome **{${e.nome}}** está no nível **{${level}}** com o xp : ${e.xp} `);
+    
+})
+
